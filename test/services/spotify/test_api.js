3622d979c5060c7fce3a742aeafa9c08bf55c634
chrisdevwords <chrisedwards@Chriss-MacBook-Pro.local>
2017-03-04 17:27:45 -0500

    adding spec for spotify api track info

--- test/services/spotify/test_api.js
+++ test/services/spotify/test_api.js
@@ -34,22 +34,91 @@ describe('The Spotify API Helper', () => {
         });
     });
 
-    describe.skip('findTrack', () => {
+    describe('findTrack', () => {
         context('with a uri for a Spotify Track', () => {
             it('fetches the artist', (done) =>{
-                done();
+                findTrack('19oPsdlHwigJm2Ewk1ypRb')
+                    .then((track) => {
+                        expect(track.artist)
+                            .to.eq('Eddie Rabbitt');
+                        done();
+                    })
+                    .catch(done);
             });
 
-            it('fetches the track name', (done) =>{
-                done();
+            it('fetches multiple artists', (done) => {
+                findTrack('0wFkjAM4VVoepQdYB6kl4U')
+                    .then((track) => {
+                        expect(track.artist)
+                            .to.eq('Prince, Eric Leeds')
+                        done();
+                    })
+                    .catch(done);
+            });
+
+            it('fetches the track name', (done) => {
+                findTrack('6tASfEUyB7lE2r6DLzURji')
+                    .then((track) => {
+                        expect(track.name)
+                            .to.eq('Club Tropicana')
+                        done();
+                    })
+                    .catch(done);
+            });
+
+            it('fetches the uri', (done) => {
+                findTrack('6tASfEUyB7lE2r6DLzURji')
+                    .then((track) => {
+                        expect(track.uri)
+                            .to.eq('spotify:track:6tASfEUyB7lE2r6DLzURji');
+                        done();
+                    })
+                    .catch(done);
+            });
+        });
+
+        context('with an invalid track id', () => {
+            it('throws a 400', (done) => {
+                findTrack('foo')
+                    .then(() => {
+                        done(Error('Error should be thrown.'))
+                    })
+                    .catch(({ statusCode }) => {
+                        expect(statusCode)
+                            .to.eq(400);
+                        done();
+                    })
+                    .catch(done);
+            });
+        });
+
+        context('with a valid but non-existent track id', () => {
+            it('throws a 404', (done) => {
+                findTrack('xxxxxxxxxxxxxxxxxxxxxx')
+                    .then(() => {
+                        done(Error('Error should be thrown.'))
+                    })
+                    .catch(({ statusCode }) => {
+                        expect(statusCode)
+                            .to.eq(404);
+                        done();
+                    })
+                    .catch(done);
             });
         });
 
         context('with a uri for a track not playable in the US', () => {
-            const uri = 'spotify:track:6ny5w1J69fN2ztPl7tGYZX';
-            it.skip('throws an 405', (done) => {
-
-                done();
+            it('throws a 405', (done) => {
+                findTrack('6ny5w1J69fN2ztPl7tGYZX')
+                    .then(() => {
+                        done(Error('Error should be thrown.'))
+                    })
+                    .catch(({ statusCode }) => {
+                        expect(statusCode)
+                            .to.eq(405);
+                        done();
+                    })
+                    .catch(done);
             });
         });
     });

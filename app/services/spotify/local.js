0f5987123edf3f3f9fd2c9288c1f319ac1cf3676
chrisdevwords <chrisedwards@Chriss-MacBook-Pro.local>
2017-03-04 09:08:28 -0500

    fixing queue playback

--- app/services/spotify/local.js
+++ app/services/spotify/local.js
@@ -2,6 +2,7 @@
 const PATH = require('path');
 const applescript = require('applescript');
 
+
 const ROOT = '../../../';
 const SCRIPT_NOW_PLAYING = PATH.resolve(
     __dirname, ROOT, 'apple-script/currentsong.scpt'
@@ -13,12 +14,17 @@ const SCRIPT_PLAY = track =>
     `tell application "Spotify" to play track "${track}"`;
 const SCRIPT_PLAYLIST = pl =>
     `tell application "Spotify" to play track "${pl}"`;
-const _defaultPlaylist = 'spotify:user:spotify_uk_:playlist:2HSnhAB2ugTyXcWteTOOKy';
+const URI_PLAYLIST = id => `spotify:user:spotify_uk_:playlist:${id}`;
+
+const _defaultPlaylist = {
+    // eslint-disable-next-line babel/new-cap
+    uri: URI_PLAYLIST('2HSnhAB2ugTyXcWteTOOKy'),
+    title: 'Default Playlist'
+};
 const _queue = [];
 let _playlist = _defaultPlaylist;
 let _currentTrack;
 let _timer;
-
 
 function execString(script) {
     return new Promise((resolve, reject) => {
@@ -29,7 +35,11 @@ function execString(script) {
                 resolve(data);
             }
         };
-        applescript.execString(script, cb);
+        try {
+            applescript.execString(script, cb);
+        } catch (err) {
+            reject(err);
+        }
     });
 }
 
@@ -42,7 +52,11 @@ function execFile(file) {
                 resolve(data);
             }
         };
-        applescript.execFile(file, cb);
+        try {
+            applescript.execFile(file, cb);
+        } catch (err) {
+            reject(err);
+        }
     });
 }
 
@@ -55,11 +69,6 @@ function nowPlaying() {
                 name,
                 artist
             };
-        })
-        .then((currentTrack) => {
-            start();
-            //console.log('currentTrack', currentTrack);
-            return currentTrack;
         });
 }
 
@@ -74,38 +83,36 @@ function playTrack(track) {
 
 function shufflePlaylist(playlist) {
     // eslint-disable-next-line babel/new-cap
-    return execString(SCRIPT_PLAYLIST(playlist))
-       .then((data) => {
-           // console.log('shuffling playlist', data);
+    return execString(SCRIPT_PLAYLIST(playlist.uri))
+       .then(() => {
            _playlist = playlist;
-           // todo if this works, return the current playing track
-           return data;
+           return nowPlaying();
        });
 }
 
 function checkCurrentTrack() {
-    nowPlaying()
+    return nowPlaying()
         .then((trackInfo) => {
             if (!trackInfo.uri) {
-                //console.log('need to play playlist', trackInfo);
                 return shufflePlaylist(_playlist)
-                    .then(nowPlaying); // todo should run on a timeout
+                    .then(nowPlaying);
             }
             if (!_currentTrack) {
-                _currentTrack = Object.assign({
-                    requestedBy: 'Default Playlist',
+                _currentTrack = Object.assign(
+                    { requestedBy: _playlist.title },
                     trackInfo
-                });
+                );
             } else if (_currentTrack.uri !== trackInfo.uri) {
                 if (_queue.length) {
                     // tell spotify to play the next queued track
                     return playTrack(_queue.shift())
+                        .then(nowPlaying)
                 }
                 // set the current track to what's current playing
-                _currentTrack = Object.assign({
-                    requestedBy: 'Default Playlist',
+                _currentTrack = Object.assign(
+                    { requestedBy: _playlist.title },
                     trackInfo
-                });
+                );
             }
             return _currentTrack;
         });
@@ -120,8 +127,17 @@ function queueTrack(track) {
 }
 
 function start() {
-    clearInterval(_timer);
-    _timer = setTimeout(checkCurrentTrack, 250);
+    clearTimeout(_timer);
+    _timer = setTimeout(() => {
+        checkCurrentTrack()
+            .then((/*currentTrack*/) => {
+                start();
+            })
+            .catch((err) => {
+                // eslint-disable-next-line no-console
+                console.log('Playback error:', err);
+            });
+    }, 250);
 }
 
 module.exports = {

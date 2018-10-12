ad008b8051cff8803e8850c51fe68318c471426d
Edwards, Chris <Christopher.Edwards@nike.com>
2018-10-12 16:19:55 -0400

    #35 adding a quit button to the web FE that kills the server

--- app/public/src/js/main.js
+++ app/public/src/js/main.js
@@ -1,2 +1,18 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const io = require('socket.io-client');
 require('./spotify');
 require('./youtube');
+
+function connectSocket() {
+    const socket = io(`http://${window.location.host}/main`);
+    const quitBtn = document.getElementById('quit-button');
+    quitBtn.addEventListener('click', () => {
+        // eslint-disable-next-line no-alert
+        if (window.confirm('Are you sure you want to quit?')) {
+            socket.emit('quit');
+            window.location = 'http://www.spacejam.com';
+        }
+    });
+}
+
+connectSocket();
